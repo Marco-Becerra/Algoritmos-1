3ff85ff2dcbe750384f2d38f2c9ab13ecb733829
Marco Becerra <N00265668@upn.pe>
2026-05-21 17:06:46 -0500

    ejercicio

--- CSHARP/ejercicio1/ejercicio1/Program.cs
+++ CSHARP/ejercicio1/ejercicio1/Program.cs
@@ -10,6 +10,41 @@ namespace ejercicio1
     {
         static void Main(string[] args)
         {
+            //Fecha y Hora
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Becerra Quispe, Marco");//programador 
+            Console.WriteLine("Ejercicio 1 - Edad de una persona");
+
+            //Declara variabls
+            int edad;
+
+            //Entrada de datos
+            //mostrar mensaje al usuario para ingresar datos
+            Console.WriteLine("Ingrese su edad: ");
+            //Primera forma de guardar el valor ingresado por el usuario
+            //usando int.Parse
+            edad = int.Parse(Console.ReadLine());
+
+            //Decision
+            /* si edad>=18 entonces{
+             *   mostrar "Eres mayor de edad"
+             *   }
+             *   sino else{
+             *   mostrar "Eres menor de edad"
+             *   }
+             *   finsi
+             */
+            if (edad >= 18)
+            {
+                Console.WriteLine("Eres mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("Eres menor de edad");
+            }
+            //detener pantalla
+            Console.ReadKey();
+        }
+
         }
     }
-}
